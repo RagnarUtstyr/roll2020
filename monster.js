221d01d53bdcf897ae7db3f9a7e7dc4f44acbcd0
RagnarUtstyr <ragnarboe88@gmail.com>
2024-10-14 15:28:27 +0200

    da

--- monster.js
+++ monster.js
@@ -19,48 +19,26 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 // Function to handle adding a monster to the list
-function addToList(name, health, url) {
-    console.log(`Adding monster: ${name} with HP: ${health} and URL: ${url}`);
+function addToList(name, health, url, ac) {
+    console.log(`Adding monster: ${name} with HP: ${health}, AC: ${ac}, and URL: ${url}`);
     const initiative = prompt(`Enter initiative for ${name}:`);
     if (initiative !== null && !isNaN(initiative)) {
-        submitMonsterToFirebase(name, parseInt(initiative), health, url);
+        submitMonsterToFirebase(name, parseInt(initiative), health, url, ac);
     } else {
         alert('Please enter a valid initiative number.');
     }
 }
 
 // Function to submit data to Firebase
-async function submitMonsterToFirebase(name, initiative, health, url) {
+async function submitMonsterToFirebase(name, initiative, health, url, ac) {
     try {
         console.log('Attempting to push data to Firebase...');
         const reference = ref(db, 'rankings/');
-        const newEntryRef = await push(reference, { name, number: initiative, health, url });
+        await push(reference, { name, number: initiative, health, url, ac });
         console.log('Data pushed to Firebase successfully.');
-
-        // If push is successful, add to the UI
-        addMonsterToListUI(newEntryRef.key, name, initiative, health, url);
     } catch (error) {
-        console.error('Error (possibly network-related) submitting monster, but continuing:', error);
-        // No alert, silent logging instead
+        console.error('Error submitting monster:', error);
     }
-}
-
-// Function to add monster to the list UI
-function addMonsterToListUI(id, name, initiative, health, url) {
-    const rankingList = document.getElementById('rankingList');
-    if (!rankingList) {
-        console.error("Ranking list element not found in the DOM. Cannot add monster.");
-        return;
-    }
-
-    const listItem = document.createElement('li');
-    listItem.textContent = `${name} (Int: ${initiative}, HP: ${health})`;
-    listItem.className = 'list-item';
-    listItem.dataset.url = url;
-    listItem.onclick = function() {
-        window.open(this.dataset.url, '_blank');
-    };
-    rankingList.appendChild(listItem);
 }
 
 // Attach addToList function to the global window object to be accessible from the HTML
